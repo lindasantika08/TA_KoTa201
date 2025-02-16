3bc1bae26c4e4206dee2ee4cb8793ac93350a9ba
adhiyarahma <adhiyarahma887@gmail.com>
2025-02-16 17:47:36 +0700

    update seeder prodi n class

--- database/seeders/ClassSeeder.php
+++ database/seeders/ClassSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\Prodi;
 
 class ClassSeeder extends Seeder
 {
@@ -13,17 +14,19 @@ class ClassSeeder extends Seeder
      */
     public function run(): void
     {
+        $prodi = Prodi::first();
+
         DB::table('class_room')->insert([
             [
                 'id' => 'a7e6ad5a-a8e7-4654-aa65-1a3301a394cl',
                 'class_name' => 'B',
-                'prodi_id' =>'a0e6ad5a-a8e7-4654-aa65-1a3301a394cl',
+                'prodi_id' =>$prodi->id,
                 'angkatan' => '2022',
             ],
             [
                 'id' => 'be6ad5a-a8e7-4654-aa65-1a3301a394cl',
                 'class_name' => 'A',
-                'prodi_id' =>'a0e6ad5a-a8e7-4654-aa65-1a3301a394cl',
+                'prodi_id' => $prodi->id,
                 'angkatan' => '2022',
             ],
         ]);
