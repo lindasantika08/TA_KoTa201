4167574f94858f87a9caba8360e6ea2f1b543d6f
tmfadhli12 <thoriqmf123@gmail.com>
2025-03-17 14:42:36 +0700

    prod

--- prod-entrypoint.sh
+++ prod-entrypoint.sh
@@ -1,44 +1,22 @@
-#!/bin/bash
+#!/bin/sh
 
-# Fungsi untuk menunggu database siap
+export $(grep -v '^#' .env | xargs)
+
 echo "Menunggu database siap..."
 until php -r "try { new PDO('mysql:host=${DB_HOST};dbname=${DB_DATABASE}', '${DB_USERNAME}', '${DB_PASSWORD}'); echo 'Database siap.'; } catch (PDOException \$e) { exit(1); }"; do
     sleep 3
     echo "Menunggu database..."
 done || { echo "Gagal terhubung ke database."; exit 1; }
+echo "Database siap."
 
-# Jalankan migrasi
-echo "Menjalankan migrasi..."
-if php artisan migrate --force; then
-    echo "Migrasi berhasil."
-else
-    echo "Gagal menjalankan migrasi."
-    if php artisan db:wipe; then
-        echo "Membersihkan database berhasil."
-    else
-        echo "Gagal Membersihkan database."
-        exit 1
-    fi
+if [ "$RUN_MIGRATIONS" = "true" ]; then
+  if php artisan migrate:status | grep -q 'Pending'; then
+    php artisan migrate --force
+    php artisan db:seed --force
+  else
+    echo "Tidak ada migrasi yang perlu dijalankan."
+  fi
 fi
 
-# Jalankan seeder
-if [ "${RUN_SEEDER}" = "true" ]; then
-    echo "Menjalankan seeder..."
-    if php artisan db:seed --force; then
-        echo "Seeder berhasil."
-    else
-        echo "Gagal menjalankan seeder."
-        if php artisan migrate:refresh --seed; then
-            echo "Melakukan migrate refresh berhasil."
-        else
-            echo "Gagal Melakukan migrate refresh."
-            exit 1
-        fi
-    fi
-else
-    echo "Seeder tidak dijalankan (RUN_SEEDER=false)."
-fi
-
-# Jalankan server Laravel
-echo "Menjalankan server..."
-exec "/usr/bin/supervisord", "-c", "/etc/supervisord.conf" --force
+# Start supervisord
+exec /usr/bin/supervisord -c /etc/supervisord.conf
