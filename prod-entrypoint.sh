803a30603863a770cb42b1bf8ee1fe81de214d18
tmfadhli12 <thoriqmf123@gmail.com>
2025-03-17 17:11:06 +0700

    worker

--- prod-entrypoint.sh
+++ prod-entrypoint.sh
@@ -19,5 +19,4 @@ if [ "$RUN_MIGRATIONS" = "true" ]; then
 fi
 
 # Start supervisord
-exec php artisan serve
-# exec /usr/bin/supervisord -c /etc/supervisord.conf
+exec /usr/bin/supervisord -c /etc/supervisord.conf
