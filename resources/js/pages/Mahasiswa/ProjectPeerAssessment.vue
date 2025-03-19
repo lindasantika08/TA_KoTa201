4a3b634bc06360e688f85ad0ecef1275de9c1c28
lindasantika08 <linda.santika.tif22@polban.ac.id>
2025-03-20 00:32:43 +0700

    fixed prefix

--- resources/js/pages/Mahasiswa/ProjectPeerAssessment.vue
+++ resources/js/pages/Mahasiswa/ProjectPeerAssessment.vue
@@ -19,7 +19,7 @@ export default {
   data() {
     return {
       breadcrumbs: [
-        { text: "Assessment", href: "/mahasiswa/assessment/peer" },
+        { text: "Assessment", href: "/sispa/mahasiswa/assessment/peer" },
         { text: "Peer Assessment", href: null }
       ],
       headers: [
