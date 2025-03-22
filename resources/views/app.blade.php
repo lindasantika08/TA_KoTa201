5fd48c9bb6f375fee66babcf8835c6735c45502e
tmfadhli12 <thoriqmf123@gmail.com>
2025-03-22 21:52:10 +0700

    change friom png to ico

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+  <link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}">
 
   <title inertia>{{ config('app.name', 'Laravel') }}</title>
   @vite(['resources/css/app.css', 'resources/js/app.js'])
