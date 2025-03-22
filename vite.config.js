4cdf8b46847f94a94337bfce40698aeac2f2d6cd
tmfadhli12 <thoriqmf123@gmail.com>
2025-03-22 17:45:54 +0700

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
+    base: '/sispa/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
