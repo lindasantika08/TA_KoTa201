a720f724c98163b7eb5c21506b9cd3d41b571d2f
lindasantika08 <linda.santika.tif22@polban.ac.id>
2025-03-23 21:34:42 +0700

    hash

--- app/Imports/DosenImport.php
+++ app/Imports/DosenImport.php
@@ -80,7 +80,7 @@ class DosenImport implements ToModel, WithHeadingRow, WithBatchInserts, OnEachRo
                             'id' => Str::uuid(),
                             'name' => $row['name'],
                             'email' => $row['email'],
-                            'password' => $password, // No hashing applied
+                            'password' => bcrypt($password), // No hashing applied
                             'role' => 'dosen'
                         ]);
                     }
@@ -102,7 +102,7 @@ class DosenImport implements ToModel, WithHeadingRow, WithBatchInserts, OnEachRo
                             'id' => Str::uuid(),
                             'name' => $row['name'],
                             'email' => $row['email'],
-                            'password' => $password, // No hashing applied
+                            'password' => bcrypt($password), // No hashing applied
                             'role' => 'dosen'
                         ]);
                     }
