8a51b6ac9f8517e30c59b623fedaa3d32497595d
tmfadhli12 <thoriqmf123@gmail.com>
2025-06-19 15:35:57 +0700

    Update NGINX

--- prod-entrypoint.sh
+++ prod-entrypoint.sh
@@ -13,12 +13,6 @@ if [ "$RUN_MIGRATIONS" = "true" ]; then
   php artisan migrate:fresh --seed --force
 fi
 
-if [ ! -d /var/www/public/build ]; then
-  echo "Running npm build..."
-  npm install
-  npm run build
-fi
-
 # Start supervisord
 
 exec /usr/bin/supervisord -c /etc/supervisord.conf
