ebf22419c71601744df1541c618182e7694cb351
lindasantika08 <linda.santika.tif22@polban.ac.id>
2025-07-14 11:23:57 +0700

    sapa

--- resources/js/Components/NavbarAdmin.vue
+++ resources/js/Components/NavbarAdmin.vue
@@ -184,7 +184,7 @@ export default {
 
 <template>
   <nav class="bg-white text-black py-4 px-6 flex justify-between items-center sticky top-0 w-full z-10 shadow-md">
-    <div class="text-xl font-bold">Assessment App</div>
+    <div class="text-xl font-bold">SAPA</div>
     <div class="flex items-center space-x-4">
       <!-- <button aria-label="Notifications" class="relative group focus:outline-none"
         @click="goToNotifications">
